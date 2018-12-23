5f3c1b1a3b89d17e70505bb91eb1df20e9c85744
Philippe Schaeffer <schaeff-github@compuphil.de>
2018-12-23 19:07:42 +0100

    Changed Install Script to new oracle-ict version

--- install-ict_debian.sh
+++ install-ict_debian.sh
@@ -1,61 +1,85 @@
 #/bin/sh
+
+ICTHOME="/home/ict"
+ICTDIR="oracle-ict"
+GITREPO="iotaledger/ict"
+NEIGHBORS="127.0.0.1:14265,127.0.0.2:14265,127.0.0.3:14265"
+
+if [ -z "$1" ] || [ "$1" != "BUILD" -a "$1" != "RELEASE" ] ; then
+	echo 'Please choose between BUILD or RELEASE:'
+    echo './$0 [BUILD|RELEASE]'
+    exit
+fi
+
 apt-get update
 apt-get upgrade -y
-apt-get install gnupg dirmngr -y --install-recommends
-grep "^deb .*webupd8team" /etc/apt/sources.list || echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" >> /etc/apt/sources.list
-grep "^deb-src .*webupd8team" /etc/apt/sources.list || echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" >> /etc/apt/sources.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C2518248EEA14886
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
-apt-get update
-apt-get upgrade -y
-echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections
-apt-get install git oracle-java8-installer oracle-java8-set-default -y --allow-unauthenticated
-useradd -d /home/ict -m -s /bin/bash ict
-cd /home/ict
-git clone https://github.com/Come-from-beyond/Ict.git
-cd /home/ict/Ict
-javac /home/ict/Ict/src/cfb/ict/*.java
+apt-get install curl -y --install-recommends
+VERSION=`curl --silent "https://api.github.com/repos/${GITREPO}/releases" | grep '"tag_name":' |head -1 | sed -E 's/.*"([^"]+)".*/\1/'`
 
-cat <<EOF > /home/ict/Ict/run-ict.sh
+useradd -d ${ICTHOME} -m -s /bin/bash ict
+mkdir -p ${ICTHOME}/${ICTDIR}
+cd ${ICTHOME}/${ICTDIR}
+
+if [ "$1" == "BUILD" ]; then
+	apt-get install git gnupg dirmngr gradle -y --install-recommends
+	grep "^deb .*webupd8team" /etc/apt/sources.list || echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" >> /etc/apt/sources.list
+	grep "^deb-src .*webupd8team" /etc/apt/sources.list || echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" >> /etc/apt/sources.list
+	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C2518248EEA14886
+	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
+	apt-get update
+	apt-get upgrade -y
+	echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
+	echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections
+	apt-get install oracle-java8-installer oracle-java8-set-default -y --allow-unauthenticated
+
+	git clone https://github.com/${GITREPO}
+	cd ${ICTHOME}/${ICTDIR}/ict
+	gradle fatJar
+fi
+
+if [ "$1" == "RELEASE" ]; then
+	if [ ! -f ict/ict-${VERSION}.jar ]; then
+			mkdir ict
+			cd ict
+			curl -O https://github.com/iotaledger/ict/releases/download/${VERSION}/ict-${VERSION}.jar
+	fi
+fi
+
+
+cat <<EOF > ${ICTHOME}/run-ict.sh
 #!/bin/bash
-cd /home/ict/Ict/src
-java cfb.ict.Ict /home/ict/config/ict.properties
+cd ${ICTHOME}/${ICTDIR}
+java -jar ${VERSION}.jar -c ${ICTHOME}/config/ict.cfg
 EOF
 
-echo <<EOF > /home/ict/Ict/update-ict.sh
-#!/bin/bash
-git pull
-javac src/cfb/ict/*.java
-systemctl restart ict
+chmod a+x ${ICTHOME}/run-ict.sh
+
+mkdir -p ${ICTHOME}/config
+
+if [ ! -f ${ICTHOME}/config/ict.cfg ]; then
+	cat <<EOF > ${ICTHOME}/config/ict.cfg
+name=ict
+ixis=
+port=14265
+log_round_duration=60000
+ixi_enabled=false
+spam_enabled=false
+min_forward_delay=0
+host=0.0.0.0
+neighbors=${NEIGHBORS}
+max_forward_delay=200
 EOF
+fi
 
-chmod a+x run-ict.sh update-ict.sh
-
-mkdir -p /home/ict/config
-cat <<EOF > /home/ict/config/ict.properties
-host = 0.0.0.0
-port = 14265
-//Discord neighbor:
-neighborAHost = 127.0.0.1
-neighborAPort = 14265
-//Discord neighbor:
-neighborBHost = 127.0.0.2
-neighborBPort = 14265
-//Discord neighbor:
-neighborCHost = 127.0.0.3
-neighborCPort = 14265
-EOF
-
-chown -R ict /home/ict/config /home/ict/Ict
+chown -R ict ${ICTHOME}/config ${ICTHOME}/${ICTDIR}
 
 cat <<EOF > /lib/systemd/system/ict.service
 [Unit]
 Description=IOTA ICT
 After=network.target
 [Service]
-ExecStart=/bin/bash -u run-ict.sh
-WorkingDirectory=/home/ict/Ict
+ExecStart=/bin/bash -u ${ICTHOME}/run-ict.sh
+WorkingDirectory=${ICTHOME}/${ICTDIR}
 StandardOutput=inherit
 StandardError=inherit
 Restart=always
@@ -65,6 +89,9 @@ WantedBy=multi-user.target
 EOF
 
 systemctl enable ict
-echo "2 22 * * * systemctl restart ict" >> /var/spool/cron/crontabs/root
-systemctl start ict
-journalctl -u ict
+if [ `grep -c "systemctl restart ict" /var/spool/cron/crontabs/root` -eq 0 ]; then
+	echo "2 22 * * * systemctl restart ict" >> /var/spool/cron/crontabs/root
+fi
+
+systemctl restart ict
+journalctl -fu ict
